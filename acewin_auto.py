9326fb8bb69bcb4d842e1178c2a1f150a46bb7ab
tamizh-py <tamizharasandk@gmail.com>
2026-03-02 01:49:14 +0530

    Update acewin_auto.py

--- acewin_auto.py
+++ acewin_auto.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 import time
+import os
 
 # Configure headless Chrome
 options = Options()
@@ -22,9 +23,11 @@ driver.get("https://www.acewin.in/login")
 # -------- LOGIN SECTION --------
 wait.until(EC.presence_of_element_located((By.ID, "phone")))
 
-driver.find_element(By.ID, "phone").send_keys("7305376541")
-driver.find_element(By.ID, "password").send_keys("Dkttamil7@")
+USERNAME = os.getenv("PHONE")
+PASSWORD = os.getenv("PASSWORD")
 
+driver.find_element(By.ID, "phone").send_keys(USERNAME)
+driver.find_element(By.ID, "password").send_keys(PASSWORD)
 # Click login using fresh locate + JS
 wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
 driver.execute_script(
@@ -71,3 +74,4 @@ print("Clicked second button")
 time.sleep(15)
 
 driver.quit()
+
